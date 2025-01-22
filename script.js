1b72f05c4bc4c4503f849cd66a3f21aa7d95fbf9
tix-flix <timatialiev7@gmail.com>
2025-01-22 16:56:59 +0600

    first

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-let time = $(".data"),
-data = $(".data2");
+$.ajax({
+    type: "GET",
+    url: "https://narutodb.xyz/api/character?page=25",
+    success: function (response) {
+        response.characters.map(element => {
+            console.log(response)
 
-setInterval(() => {
-    let wd = new Date();
-   
-    let h = wd.getHours();
-    let m = wd.getMinutes();
-    let s = wd.getSeconds();
-
-    let day = wd.getDay();
-    let wdate = wd.getDate();
-    let month = wd.getMonth();
-    let y = wd.getFullYear();
-
-    time.text(`${h}:${zero(m)}:${zero(s)}`);
-    data.text(`${dni[day]}, ${zero(wdate)} ${months[month]}, ${y}`);
-
-}, 1000);
-
-function zero(x) {
-    if (x < 10) {
-        return `0${x}`;
-    } else {
-        return x;
+            let card = `
+            <div class="col-md-6 mb-4 d-flex justify-content-center ">
+                <div class="card bgc__col mb-3" style="width: 18rem">
+                        <img src="${element.images}" class="card-img-top" width="220" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">${element.name}</h5>
+                            <p class="card-text">Jutsu: ${element.jutsu}</p>
+                        </div>
+                    </div>
+            </div>                
+            `;
+            $("#cardsContainer").append(card);
+        });
     }
-}
-
-let dni = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+});
